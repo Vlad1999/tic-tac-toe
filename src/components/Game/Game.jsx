c8a8b068cd526d84ecfb0fe164a43c4dc886148d
Всеволод Грудина <vsevolodgrudina@Vsevolods-MacBook-Pro.local>
2022-08-10 10:16:38 +0300

    feat: integrate with bizzabo sdk a bit

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -2,6 +2,9 @@ import "./Game.css";
 import { useState } from "react";
 import Board from "../Board/Board";
 import Scores from "../Scores/Scores";
+import { BizzaboUiActions } from '@bizzabo/dev-platform-web-sdk';
+
+// b4bEp3UDIJ9KVcUBEYefdNHDXIowaCkr
 
 function Game() {
   
@@ -11,10 +14,22 @@ function Game() {
     tie: 0,
   });
 
+  const bizzaboWebSdk = new BizzaboUiActions('b4bEp3UDIJ9KVcUBEYefdNHDXIowaCkr', 'https://vh-attendees.ext.dev.bizzabo.com');
+
+  const onClick = (e) => {
+    bizzaboWebSdk.openLinkInNewTab('https://dashboard.pigeonholelive.com/help/create');
+  }
+
   return (
     <div className="Game">
       <Board setScores={setScores}/>
       <Scores scores={scores}/>
+      <div
+        onClick={onClick}
+        style={{cursor: 'pointer'}}
+        className="scores">
+        contact us please
+      </div>
     </div>
   );
 }
